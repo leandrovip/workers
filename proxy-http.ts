4b2350d42d1db53ff0e2adf49dfd08904327ec22
Leandro Ferreira <leandro@vipsolucoes.com>
2025-04-27 08:45:01 -0300

    Atualiza estrutura do worker

--- proxy-http.ts
+++
@@ -1,8 +0,0 @@
-export default {
-  async fetch(request, env, ctx) {
-    const url = new URL(request.url);
-    url.port = "8080"; // Define a porta 8080
-    url.protocol = "http:"; // Garante que vai HTTP para o servidor interno
-    return fetch(url.toString(), request);
-  },
-};
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 export default {
   async fetch(request, env, ctx) {
     const url = new URL(request.url);
-    url.port = "8443";
-    url.protocol = "https:";
+    url.port = "8080";
+    url.protocol = "http:";
     return fetch(url.toString(), request);
   },
 };
